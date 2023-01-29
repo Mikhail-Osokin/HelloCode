44763964de7c91387260366f60d3a67f6a975081
Mikhail <un.less.jk@gmail.com>
2023-01-29 15:52:39 +0300

    add Second

---
+++ Second/Program.cs
@@ -0,0 +1,16 @@
+﻿/* напишите программу, которая на воход принимает число 
+и вывает его квадрат (число, умноженое само на себя)
+
+Наример:
+4 -> 16
+3 -> 9
+-7 -> 49
+*/
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine() ?? ""; //12243324 апывываваа 3553445345
+int number = Convert.ToInt32(numberStr);
+int result = number*number;
+Console.WriteLine($"Квадрат числа {number} равен {result}");
+
+
