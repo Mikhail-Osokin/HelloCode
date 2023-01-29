240e947e693545ce5b3f83789be513d7015138cb
Mikhail <un.less.jk@gmail.com>
2023-01-29 15:05:35 +0300

    fix Examples 007 Magic

--- Examples007_Magic/Program.cs
+++ Examples007_Magic/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Clear();
 //Console.SetCursorPosition(10,4);
 //Console.WriteLine("+");
-int xa = 1, ya = 1, xb = 1, yb = 30, xc = 60, yc = 1;
+int xa = 30, ya = 1, xb = 1, yb = 30, xc = 60, yc = 1;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -18,7 +18,7 @@ int count = 0;
 
 while (count<10000)
 {
-    int what = new Random().Next(0,3); // [0,3) 0,1,2..
+    int what = new Random().Next(0, 3); // [0,3) 0,1,2..
     if (what == 0)
     {
         x = (x + xa) / 2;
@@ -29,7 +29,7 @@ while (count<10000)
         x = (x + xb) / 2;
         y = (y + yb) / 2;    
     }
-    if (what == 1)
+    if (what == 2)
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;    
