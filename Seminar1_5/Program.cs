f2f88c3a13020e9b9de664f14ebd729551958477
Mikhail <un.less.jk@gmail.com>
2023-01-29 17:11:24 +0300

    add Seminar1_5

---
+++ Seminar1_5/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+string? numberStr = Console.ReadLine();
+if (numberStr != null)
+{
+    int number = Math.Abs(int.Parse(numberStr));
+Console.WriteLine(number%10);
+}
