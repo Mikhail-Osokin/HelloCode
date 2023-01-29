bfce5dfaf7e35cd900c1652f7974c793996467f8
Mikhail <un.less.jk@gmail.com>
2023-01-29 16:11:37 +0300

    add Seminar2

---
+++ Seminar2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число A!");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B!");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A/B == B)
+{
+    Console.WriteLine($"Число {A} является квадртом числа {B}");
+}
+else
+{
+    Console.WriteLine($"Число {A} не является квадртом числа {B}");
+}
