bf7d9861275cc84853cfb7d27b6d85fe4f78e21f
Mikhail <un.less.jk@gmail.com>
2023-01-29 16:23:46 +0300

    add Seminar1_3

---
+++ Seminar1_3/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите число от одного до семи!");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number==1)
+{
+    Console.WriteLine("Сегодня понедельник");
+}
+if (number==2)
+{
+    Console.WriteLine("Сегодня вторник");
+}
+if (number==3)
+{
+    Console.WriteLine("Сегодня среда");
+}
+if (number==4)
+{
+    Console.WriteLine("Сегодня четверг");
+}
+if (number==5)
+{
+    Console.WriteLine("Сегодня пятница");
+}
+if (number==6)
+{
+    Console.WriteLine("Сегодня суббота");
+}
+if (number==7)
+{
+    Console.WriteLine("Сегодня воскресенье");
+}
+
+
