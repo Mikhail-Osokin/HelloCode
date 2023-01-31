a204fa4ffc2b0af2296bc61704ef043e9dc4537a
Mikhail <un.less.jk@gmail.com>
2023-01-31 04:27:16 +0300

    add Seminar2_2

---
+++ Seminar2_2/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+Например:
+678->68
+567->57
+145->15
+*/
+
+int GetNumber()
+{
+    Random rmd = new Random();
+    int result = rmd.Next(100, 1000); // [left, right)
+    return result;
+}
+
+int DelSecondDigitOfNumber(int number)
+{
+    int firstDigit = number / 100;
+    int thirdDigit = number % 10;
+    return (firstDigit*10+thirdDigit);
+}
+
+int number = GetNumber();
+int result = DelSecondDigitOfNumber(number);
+
+Console.WriteLine($"полученное число после удаления второй цифры числа {number} = {result}");
