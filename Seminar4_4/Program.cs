caa203b181005903d6a532080f1529432e854f9a
Mikhail <un.less.jk@gmail.com>
2023-02-08 23:09:21 +0300

    add Seminar4_4

---
+++ Seminar4_4/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Написать программу, которая выдает массив из 8 элементов, заполненный нулями и единицами в случайном порядке
+[1,0,1,0,0,1,1,0]
+*/
+
+int [] array = new int[8];
+void InitArray(int [] array)
+{
+    int i = 0;
+    Random rnd = new Random();
+    while (i<array.Length)
+    {
+        array[i] = rnd.Next(0,2);
+        i++;
+    }
+}
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+InitArray(array);
+PrintArray(array);
