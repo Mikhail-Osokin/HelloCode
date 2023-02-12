da2e0159bddc411e3759fca70262b68647d1fbb5
Mikhail <un.less.jk@gmail.com>
2023-02-12 12:04:04 +0300

    add Seminar5_2

---
+++ Seminar5_2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 32: Напишите программу замены элементов массива: положительные элементы заменяются на соответствующие отрицательные и наоборот
+Например: [4, 8, -8, -2] -> [-4, -8, 8, 2]
+*/
+
+int [] InitArray()
+{
+    int[] result = new int[12];
+    Random rnd = new Random();
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(-999,1000);
+    }
+    return result;
+}
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void ReplacePosNeg(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i]*(-1);
+    }
+}
+
+int [] arr = InitArray();
+PrintArray(arr);
+Console.WriteLine();
+ReplacePosNeg(arr);
+PrintArray(arr);
