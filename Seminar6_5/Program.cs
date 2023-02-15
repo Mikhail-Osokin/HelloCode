1cc07fcb15b46a441069156fe512249bc9f6bcd5
Mikhail <un.less.jk@gmail.com>
2023-02-16 01:01:34 +0300

    add Seminar6_5

---
+++ Seminar6_5/Program.cs
@@ -0,0 +1,40 @@
+﻿/* 
+Напишите программу, которая будет создавать
+копию заданного массива с помощью поэлементного копирования
+*/
+
+int [] InitArray ()
+{
+    int [] result = new int [6];
+    Random rnd = new Random();
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(1, 10);
+    }
+return result;
+}
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int [] CopyArray(int [] array)
+{
+    int [] result = new int [array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i]=array[i];
+    }
+    return result;
+}
+
+int [] arr = InitArray();
+PrintArray(arr);
+int [] result = CopyArray(arr);
+PrintArray(result);
