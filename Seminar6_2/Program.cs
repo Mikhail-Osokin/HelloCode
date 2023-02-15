f1c88c987716ddacf59d8033b465013ba490cfd1
Mikhail <un.less.jk@gmail.com>
2023-02-16 01:01:00 +0300

    add Seminar6_2

---
+++ Seminar6_2/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Напишите программу, которая принимает на вход три числа и проверяет 
+может ли существовать треугольник со сторонами такой длинны 
+(Теорема о неравенстве сторон треугольника: Сторона треугольника меньше суммы двух других сторон)
+*/
+
+int GetNumber(string message)
+
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+void CheckForTriangle(int a, int b, int c)
+{
+    if (a<b+c && b<a+c && c<a+b)
+        Console.WriteLine($"стороны с длинами {a}, {b}, {c} образуют треугольник");
+    else
+        Console.WriteLine($"стороны с длинами {a}, {b}, {c} не образуют треугольник");
+}
+
+int a = GetNumber("Введите число а: ");
+int b = GetNumber("Введите число b: ");
+int c = GetNumber("Введите число c: ");
+
+CheckForTriangle(a,b,c);
