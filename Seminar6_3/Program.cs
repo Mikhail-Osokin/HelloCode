6f92d08e01424e08bda6f6187d86bb2cb3d70f01
Mikhail <un.less.jk@gmail.com>
2023-02-16 01:01:09 +0300

    add Seminar6_3

---
+++ Seminar6_3/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Напишите программу, которая будет преобразовывать десятичное число в двоичное
+10->110
+5->11
+*/
+
+int GetNumber(string message)
+
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result >0 )
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+void NumberTranslation (int number)
+{
+    string result = string.Empty;
+    while (number>0)
+    {
+        result = result + number%2;
+        number = number/2;
+    }
+Console.WriteLine($"Двоичное представление введенного числа = {result}");
+}
+
+int number = GetNumber("Введите положительное число: ");
+NumberTranslation(number);
