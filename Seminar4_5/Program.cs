a0dbf75fa366799e2ffeba195801be3bd7123d2f
Mikhail <un.less.jk@gmail.com>
2023-02-22 00:43:13 +0300

    add Seminar4_5

---
+++ Seminar4_5/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Ввести массив через консоль
+*/
+
+Console.WriteLine("Введите числа через пробел");
+string [] strArray = Console.ReadLine().Split(' ');
+int [] result = new int[strArray.Length];
+for (int i = 0; i < result.Length; i++)
+{
+    result[i]=int.Parse(strArray[i]);
+}
+Console.WriteLine(string.Join(',', result));
