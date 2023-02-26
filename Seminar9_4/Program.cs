9a8de0597eb6fcb19ff9e90fb26a153a2142f78b
Mikhail <un.less.jk@gmail.com>
2023-02-26 21:55:11 +0300

    add Seminar9_4

---
+++ Seminar9_4/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Наишите программу, которая на вход принимает два числа
+A и B и возводит число A в целую степень B
+*/
+
+int GetNumber(string message)
+
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result>=1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некорректное число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+int Exponent(int numberA, int numberB)
+{
+    if (numberB == 1)
+    {
+        return numberA;
+    }
+    return numberA*Exponent(numberA,numberB-1);
+}
+
+int numberA = GetNumber("Введите число A: ");
+int numberB = GetNumber("Введите число A: ");
+Console.WriteLine();
+Console.Write($"число {numberA}, возведенное в степень {numberB} = {Exponent(numberA,numberB)}");
