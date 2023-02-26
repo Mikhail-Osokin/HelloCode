421ee2b7066089324d80894f7e0700b44d62bb7e
Mikhail <un.less.jk@gmail.com>
2023-02-26 21:54:58 +0300

    add Seminar9_2

---
+++ Seminar9_2/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задайте значения N и M. Напишите программу, 
+которая выведет все наутарьные числа в промежутке от N до M.
+*/
+
+int GetNumber(string message)
+
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result>=1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некорректное число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+string GetNeturalNumbers(int N, int M)
+{
+    if (M==N)
+    {
+        return N.ToString();
+    }
+    return N.ToString() + ", " + GetNeturalNumbers(N+1,M);
+    
+}
+
+int N = GetNumber("Введите первое число: ");
+int M = GetNumber("Введите первое число: ");
+string result = GetNeturalNumbers(N, M);
+Console.WriteLine(result);
