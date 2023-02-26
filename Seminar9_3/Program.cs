db8dbe13ea611e166e0abba6b387b3897089f4cf
Mikhail <un.less.jk@gmail.com>
2023-02-26 21:55:04 +0300

    add Seminar9_3

---
+++ Seminar9_3/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Напишите программу, которая будет принимать на вход число
+возвращать сумму его цифр
+Например:
+453 -> 12
+45 -> 9
+*/
+
+int GetNumber(string message)
+
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result>=1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некорректное число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+int GetSumDigitsOfNumber(int number, int sum)
+{
+    if (number == 0)
+    {
+        return sum;
+    }
+    else 
+    sum += number%10;
+    number /= 10;
+    return GetSumDigitsOfNumber(number, sum);
+}
+
+int number = GetNumber("Введите число: ");
+int sum = 0;
+int result = GetSumDigitsOfNumber(number,sum);
+Console.Write(result);
